76f8203d94d7e7193ddccae1b6073a2fc3aa5708
Edward Andrews-Hodgson <edward@aldreth.com>
2017-06-05 11:53:46 +0100

    Remove image from collapse
    references https://github.com/RaspberryPiFoundation/ups/issues/391

--- lib/kramdown_rpf/rpf.rb
+++ lib/kramdown_rpf/rpf.rb
@@ -13,15 +13,13 @@ module RPF
       def self.convert_collapse_to_html(collapse)
         collapse =~ YAML_FRONT_MATTER_REGEXP
         details = YAML.safe_load(Regexp.last_match(1))
+        title = details['title']
         content = Regexp.last_match(2).strip
         <<~HEREDOC
           <div class="panel js-collapse">
             <div class="panel-heading drawer js-collapse">
-              <div class="panel-heading-image js-collapse">
-                <img src="#{details['image']}" class="js-collapse">
-              </div>
               <div class="panel-heading-text js-collapse">
-                <h4 class="js-collapse">#{details['title']}</h4>
+                <h4 class="js-collapse">#{title}</h4>
               </div>
               <div class="panel-heading-status status-down js-collapse">
               </div>
