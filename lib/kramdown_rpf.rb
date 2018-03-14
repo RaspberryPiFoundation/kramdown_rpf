10277da032b83df36a56d929d4c35c42d600e2c0
Edward Andrews-Hodgson <edward@aldreth.com>
2018-03-14 09:29:04 +0000

    Add locales to `I18n.load_path` rather than resetting it
    Using `=` removed any other translations from I18n.

--- lib/kramdown_rpf.rb
+++ lib/kramdown_rpf.rb
@@ -4,5 +4,5 @@ require 'kramdown'
 require_relative 'kramdown_rpf/version'
 require_relative 'kramdown_rpf/kramdown'
 
-I18n.load_path = Dir[File.join(File.absolute_path('../..', __FILE__), 'locales', '*.yml')]
+I18n.load_path << Dir[File.join(File.absolute_path('../..', __FILE__), 'locales', '*.yml')]
 I18n.backend.load_translations
