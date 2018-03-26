0029dfb34d29ec14b68bb10190b168417be69bb2
Jonic Linley <jonic@codeclub.org.uk>
2018-03-26 14:44:43 +0100

    Specs updated

--- spec/kramdown_rpf_spec.rb
+++ spec/kramdown_rpf_spec.rb
@@ -1,25 +1,41 @@
 require 'spec_helper'
 
 RSpec.describe KramdownRPF do
+  KRAMDOWN_OPTIONS = {
+    input:              'KramdownRPF',
+    parse_block_html:   true,
+    syntax_highlighter: nil,
+  }.freeze
+
+  CONVERSION_TESTS = %w[
+    challenge
+    code
+    code_with_filename
+    code_with_line_numbers
+    code_with_line_highlights
+    collapse
+    collapse_in_challenge
+    collapse_music_box
+    collapse_with_space
+    hint
+    hints
+    save
+    task
+    task_with_hints
+    task_with_ingredient
+  ].freeze
+
+  I18N_TESTS = %w[
+    hints
+    save
+  ].freeze
+
   it 'has a version number' do
     expect(KramdownRPF::VERSION).not_to be nil
   end
 
-  fdescribe 'conversions' do
-    %w[
-      challenge
-      code_filename
-      collapse
-      collapse_in_challenge
-      collapse_music_box
-      collapse_with_space
-      hint
-      hints
-      save
-      task
-      task_with_hints
-      task_with_ingredient
-    ].each do |test_name|
+  describe 'conversions' do
+    CONVERSION_TESTS.each do |test_name|
       context test_name do
         reference_result = File.read "examples/#{test_name}.html"
 
@@ -28,8 +44,7 @@ RSpec.describe KramdownRPF do
 
           test_result = Kramdown::Document.new(
             File.read("examples/#{test_name}.md"),
-            parse_block_html: true,
-            input: 'KramdownRPF'
+            KRAMDOWN_OPTIONS
           ).to_html
 
           expect(test_result.strip).to eq(reference_result.strip)
@@ -39,20 +54,18 @@ RSpec.describe KramdownRPF do
   end
 
   describe 'i18n' do
-    %w[
-      hints
-      save
-    ].each do |test_name|
+    I18N_TESTS.each do |test_name|
       context test_name do
         reference_result = File.read "examples/i18n/#{test_name}.html"
 
         it 'should be correctly converted' do
           I18n.locale = 'xyz'
+
           test_result = Kramdown::Document.new(
             File.read("examples/i18n/#{test_name}.md"),
-            parse_block_html: true,
-            input: 'KramdownRPF'
-            ).to_html
+            KRAMDOWN_OPTIONS
+          ).to_html
+
           expect(test_result.strip).to eq(reference_result.strip)
         end
       end
